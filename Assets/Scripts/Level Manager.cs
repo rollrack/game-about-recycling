edf2e04437b0eeddc3223455208ef05e2e7b1fb9
bigIq <viktorio.k.milanov.2019@elsys-bg.org>
2021-03-12 22:45:18 +0200

    playerMovement

---
+++ Assets/Scripts/Level Manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public LevelManager instance;
+    [Header("Currency")]
+    public int currency = 0;
+    public Text currencyUI;
+
+    public void IncreaseCurrency(int amount)
+    {
+        currency += amount;
+        currencyUI.text = ":wastebasket:" + currency;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float movementSpeedMultiplier;
+    public float jumpForce = 20f;
+    float mw;
+    float horizontalMove = 0f;
+    public LayerMask groundLayers;
+    public Transform feet;
+    void Update()
+    {
+        horizontalMove = mw = Input.GetAxisRaw("Horizontal");
+        {
+            Vector2 movement = new Vector2(mw * movementSpeedMultiplier, rb.velocity.y);
+            rb.velocity = movement;
+        }
+
+        Vector3 characterScale = transform.localScale;
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            characterScale.x = -2;
+        }
+
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            characterScale.x = 2;
+        }
+
+        transform.localScale = characterScale;
+    }
+    void FixedUpdate()
+    {
+
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            Jump();
+        }
+
+        bool isGrounded()
+        {
+            Collider2D groundcheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+            if(groundcheck.gameObject != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        void Jump()
+        {
+            Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+            rb.velocity = movement;
+        }
+    }
+}

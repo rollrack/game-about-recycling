f8aefef8bd1f0282423b67390700b1e0e7d163c6
rollrack <80477158+rollrack@users.noreply.github.com>
2021-03-14 00:26:35 +0200

    site

---
+++ script.js
@@ -0,0 +1,3 @@
+let buttonAnim = function () {
+    document.getElementsByTagName("button").style.backgroundColor = "animation: color-change-2x-2r 0.3s linear alternate both; @keyframes color-change-2x-2-r {0% {background: rgb(100, 182, 19);} 100% {background: chartreuse;}}"
+}
